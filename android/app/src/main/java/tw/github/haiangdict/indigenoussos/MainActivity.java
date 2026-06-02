00b608e30d0cb674c8d3945c1ae25f0b6d1c10bc
sihutongtw <haiang.dict@gmail.com>
2026-06-02 12:43:59 +0800

    Log-in problems

--- android/app/src/main/java/tw/github/haiangdict/indigenoussos/MainActivity.java
+++ android/app/src/main/java/tw/github/haiangdict/indigenoussos/MainActivity.java
@@ -1,13 +1,38 @@
 package tw.github.haiangdict.indigenoussos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.PluginHandle;
 import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
+import ee.forgr.capacitor.social.login.GoogleProvider;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+
+  @Override
+  public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
+    // Required by ModifiedMainActivityForSocialLoginPlugin interface
+  }
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     registerPlugin(SocialLoginPlugin.class);
     super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN
+        && requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+      PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
+      if (pluginHandle != null) {
+        SocialLoginPlugin plugin = (SocialLoginPlugin) pluginHandle.getInstance();
+        if (plugin != null) {
+          plugin.handleGoogleLoginIntent(requestCode, data);
+        }
+      }
+    }
   }
 }
