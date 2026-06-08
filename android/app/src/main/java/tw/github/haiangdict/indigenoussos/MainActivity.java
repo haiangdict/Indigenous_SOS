4cbc1f0d54903b42c57179587126a2714978e7fc
sihutongtw <haiang.dict@gmail.com>
2026-06-09 00:08:36 +0800

    Update MainActivity.java

--- android/app/src/main/java/tw/github/haiangdict/indigenoussos/MainActivity.java
+++ android/app/src/main/java/tw/github/haiangdict/indigenoussos/MainActivity.java
@@ -1,38 +1,38 @@
 package tw.github.haiangdict.indigenoussos;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.PluginHandle;
 import ee.forgr.capacitor.social.login.SocialLoginPlugin;
-import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
 
-  @Override
-  public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
-    // Required by ModifiedMainActivityForSocialLoginPlugin interface
-  }
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(SocialLoginPlugin.class);
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override
-  public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN
-        && requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
-      PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
-      if (pluginHandle != null) {
-        SocialLoginPlugin plugin = (SocialLoginPlugin) pluginHandle.getInstance();
-        if (plugin != null) {
-          plugin.handleGoogleLoginIntent(requestCode, data);
-        }
-      }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(SocialLoginPlugin.class);
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(true);
     }
-  }
+
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN
+                && requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+            PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
+            if (pluginHandle != null) {
+                SocialLoginPlugin plugin = (SocialLoginPlugin) pluginHandle.getInstance();
+                if (plugin != null) {
+                    plugin.handleGoogleLoginIntent(requestCode, data);
+                }
+            }
+        }
+    }
 }
